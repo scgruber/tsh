1a80d35e6b03608063d9cb1e873584fba38dd08c
Sam Gruber <grubermensch@gmail.com>
2014-06-07 23:39:53 -0400

    refactor command input

--- tsh.rs
+++ tsh.rs
@@ -1,9 +1,23 @@
 use std::io;
 
+enum Cmd {
+  Exec,
+  Builtin,
+  Error
+}
+
+fn get_cmd_from_stdin() -> Cmd{
+  let command = io::stdin().read_line();
+  match command {
+    Ok(input) => Exec,
+    Err(e) => Error
+  }
+}
+
 fn main() {
   loop {
     print!("tsh> ");
-    let command = io::stdin().read_line();
-    println!("Received command {}", command);
+    let command = get_cmd_from_stdin();
+    println!("Received command");
   }
 }
