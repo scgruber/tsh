bb01d25fe390860b77546a64f8284988c4bb9a5a
Sam Gruber <grubermensch@gmail.com>
2014-06-08 10:31:18 -0400

    add match to runtime

--- tsh.rs
+++ tsh.rs
@@ -6,6 +6,14 @@ fn main() {
   loop {
     print!("tsh> ");
     let command = cmd::parse_cmd(io::stdin().read_line());
-    println!("Received command: {}", command);
+    match command {
+      cmd::Exec(ref prog, ref args) => println!("{}", command),
+      cmd::Builtin(ref cmd) => println!("{}", command),
+      cmd::Error => {
+        println!("There was an error!");
+        return
+      },
+      cmd::Null => ()
+    }
   }
 }
