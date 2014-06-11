63e8c5952bda37629639f445c45acfaa980fa0c6
Sam Gruber <grubermensch@gmail.com>
2014-06-11 11:17:37 -0400

    implement and test push for JobsList

--- job.rs
+++ job.rs
@@ -7,6 +7,12 @@ struct Job {
   cmd: String
 }
 
+impl PartialEq for Job {
+  fn eq(&self, other: &Job) -> bool {
+    (self.jid == other.jid) && (self.pid == other.pid) && (self.cmd.equiv(&other.cmd))
+  }
+}
+
 impl fmt::Show for Job {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     write!(f, "{}\t{}\t{}", self.jid, self.pid, self.cmd)
@@ -14,19 +20,34 @@ impl fmt::Show for Job {
 }
 
 /// Record for the list of all active jobs.
-pub struct JobsList(Vec<Job>);
+pub struct JobsList{
+  jobs: Vec<Job>,
+  max_jid: int
+}
 
 impl JobsList {
   pub fn new() -> JobsList {
-    JobsList(vec![])
+    JobsList{ jobs: vec![], max_jid: 0 }
   }
+
+  pub fn push(&mut self, pid: int, cmd: String) {
+    self.jobs.push(Job{jid: self.max_jid, pid: pid, cmd: cmd});
+    self.max_jid = self.max_jid + 1;
+  }
+}
+
+#[test]
+fn jobslist_push_zeroes() {
+  let mut list = JobsList::new();
+  list.push(0, "".to_string());
+  assert_eq!(list.max_jid, 1);
+  assert_eq!(list.jobs, vec![Job{jid:0, pid:0, cmd: "".to_string()}]);
 }
 
 impl fmt::Show for JobsList {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     let mut repr: String = "JID\tPID\tCMD\n".to_string();
-    let JobsList(ref jobs) = *self;
-    for job in jobs.iter() {
+    for job in self.jobs.iter() {
       repr = repr.append(format!("{}\n", job).as_slice());
     }
     write!(f, "{}", repr)
