ab2b5bfe8fce401245c39f0b79698a6893ddcba3
Sam Gruber <grubermensch@gmail.com>
2014-06-11 11:48:45 -0400

    implement and test jid from pid

--- job.rs
+++ job.rs
@@ -20,6 +20,7 @@ impl fmt::Show for Job {
 }
 
 /// Record for the list of all active jobs.
+/// We maintain the invariant that `jobs` is a sorted vector by JID
 pub struct JobsList{
   jobs: Vec<Job>,
   max_jid: int
@@ -34,6 +35,17 @@ impl JobsList {
     self.jobs.push(Job{jid: self.max_jid, pid: pid, cmd: cmd});
     self.max_jid = self.max_jid + 1;
   }
+
+  pub fn jid_from_pid(&self, pid: int) -> Option<int> {
+    for job in self.jobs.iter() {
+      if job.pid == pid {
+        return Some(job.jid)
+      } else {
+        ()
+      }
+    }
+    return None
+  }
 }
 
 #[test]
@@ -42,6 +54,24 @@ fn jobslist_push_zeroes() {
   list.push(0, "".to_string());
   assert_eq!(list.max_jid, 1);
   assert_eq!(list.jobs, vec![Job{jid:0, pid:0, cmd: "".to_string()}]);
+}
+
+#[test]
+fn jobslist_jid_from_pid_simple() {
+  let mut list = JobsList::new();
+  list.push(1234, "foo".to_string());
+  list.push(9876, "bar".to_string());
+  list.push(1010, "baz".to_string());
+  assert_eq!(list.jid_from_pid(9876), Some(1));
+}
+
+#[test]
+fn jobslist_jid_from_pid_none() {
+  let mut list = JobsList::new();
+  list.push(1234, "foo".to_string());
+  list.push(9876, "bar".to_string());
+  list.push(1010, "baz".to_string());
+  assert_eq!(list.jid_from_pid(2468), None);
 }
 
 impl fmt::Show for JobsList {
